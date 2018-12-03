add59528d11a34a04dce32f622c3e0415e5a0e15
alex <lu77al@gmail.com>
2018-12-03 21:30:32 +0200

    Stabilized speed

--- SGTD2018/SGTD2018.asm
+++ SGTD2018/SGTD2018.asm
@@ -24,21 +24,20 @@ DriveKeyTable:	; !!! Must be within first 256 bytes of PROM
 ;HS	  High	   Dead		Halls
 .db	0b101010,0b000000 ; Not normal state (is used for key tests)
 ;000	  + + +    0 0 0
-.db	0b010010,0b010000
-;001	  - 0 +    - 0 0   Ok
-.db	0b001001,0b000001
-;010	  0 + -    0 0 -   Ok
-.db	0b011000,0b010000
-;011	  - + 0    - 0 0   Ok
-.db	0b100100,0b000100
-;100	  + - 0    0 - 0   Ok
-.db	0b000110,0b000100
-;101	  0 - +	   0 - 0   Ok
 .db	0b100001,0b000001
-;110	  + 0 -    0 0 -   Ok
+;001	  + 0 -    0 0 -   Ok
+.db	0b000110,0b000100
+;010	  0 - +    0 - 0   Ok
+.db	0b100100,0b000100
+;011	  + - 0    0 - 0   Ok
+.db	0b011000,0b010000
+;100	  - + 0    - 0 0   Ok
+.db	0b001001,0b000001
+;101	  0 + -	   0 0 -   Ok
+.db	0b010010,0b010000
+;110	  - 0 +    - 0 0   Ok
 .db	0b101010,0b000000 ; Not normal state (is used for key tests) 
 ;111	  + + +    0 0 0 
-
 
 .include "UART.inc"
 .include "Tact.inc"
