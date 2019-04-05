5766a22b974909b9e03457c01596a411b9a29426
alex <lu77al@gmail.com>
2019-04-05 22:19:49 +0300

    Restoring

--- BOOT/Boot_M8_8MHz.asm
+++ BOOT/Boot_M8_8MHz.asm
@@ -6,8 +6,8 @@
 	outi	DDRB, 0b111111
 	outi	PORTC,0b001111
 	outi	DDRC, 0b000001
-	outi	PORTD,0b11111111
-	outi	DDRD, 0b00100100
+	outi	PORTD,0b01011111
+	outi	DDRD, 0b10100100
 .endm
 //#define	UART_DIR_PORT	PORTD,2	; <- UartDir
 //#define	UART_DIR_DDR	 DDRD,2
